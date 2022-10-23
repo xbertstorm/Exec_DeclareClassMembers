4ed930ae0ac4e98f866c22de2f0d838f9fbe1547
xbertstorm5566 <xbertstorm5566@gmail.com>
2022-10-23 16:49:57 +0800

    加入專案檔案。

---
+++ Exec_DeclareClassMembers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_DeclareClassMembers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {z
+        }
+    }
+    class FileManager
+    {
+        public string TextInfo { get; set; }
+        public string Path { get; set; }
+        public void Save(string text, string path)
+        {
+
+        }
+    }
+}
